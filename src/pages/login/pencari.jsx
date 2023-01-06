f3e9f065a3100dcce627eb22df3309a49dcabd24
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2023-01-06 16:47:39 +0800

    chore: change page s structure

---
+++ src/pages/login/pencari.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LoginPencari() {
+  return <div>pencari</div>;
+}
---
+++ src/pages/login/penyedia.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LoginPenyedia() {
+  return <div>penyedia</div>;
+}
