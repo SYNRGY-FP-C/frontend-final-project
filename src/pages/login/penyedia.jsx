13f9eae5c567d8f464809808881aa8da58bfdcf5
eviindah33 <85534222+eviindah33@users.noreply.github.com>
2023-01-10 21:42:43 +0700

    add login penyedia (#7)
    * add login penyedia
    
    * add response login

--- src/pages/login/penyedia.jsx
+++ src/pages/login/penyedia.jsx
@@ -3,9 +3,32 @@ import InputWithLabel from "@/components/forms/InputWithLabel";
 import DefaultLayout from "@/layouts/DefaultLayout";
 import Section from "@/layouts/Section";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { useAuth } from "@/contexts/AuthContext";
 
+// Tambahan
 export default function LoginPenyedia() {
+  const { loginPenyedia, isLoading } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [response, setResponse] = useState({
+    isLoading: false,
+    isError: false,
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setResponse({ isLoading: true, isError: false });
+    try {
+      await loginPenyedia({ email, password });
+      setResponse({ isLoading: false, isError: true });
+      router.push("/my");
+    } catch (error) {
+      setResponse({ isLoading: false, isError: true });
+    }
+    console.log({ email, password });
+  };
+
+  // Akhir Tambahan
   return (
     <DefaultLayout title="Masuk - Penyedia">
       <Section>
@@ -16,15 +39,27 @@ export default function LoginPenyedia() {
           <div className="flex flex-col gap-y-4">
             <div className="grid grid-cols-12">
               <div className="grid col-span-12 lg:col-span-4">
-                <div className="flex flex-col gap-y-3">
-                  <InputWithLabel labelName="Email" />
-                  <InputWithLabel labelName="Password" />
+                <form className="flex flex-col gap-y-3" onSubmit={handleSubmit}>
+                  <InputWithLabel
+                    labelName="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <InputWithLabel
+                    labelName="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                   <Link href="/" className="text-xs text-center">
                     Lupa password
                   </Link>
-                  <button className="px-4 py-3 text-white rounded-lg bg-blind">
+                  <button
+                    className="px-4 py-3 text-white rounded-lg bg-blind"
+                    type="submit"
+                  >
                     Masuk
                   </button>
+
                   <div className="relative">
                     <hr className="relative h-0.5 my-4 bg-gray-200 border-0" />
                     <p className="absolute px-4 py-3 text-center transform -translate-x-1/2 -translate-y-1/2 bg-white top-1/2 left-1/2">
@@ -40,7 +75,7 @@ export default function LoginPenyedia() {
                   >
                     Saya belum memiliki akun{" "}
                   </Link>
-                </div>
+                </form>
               </div>
               <div className="hidden lg:grid md:col-span-8 place-content-center">
                 <div className="flex justify-center object-cover w-full h-full max-w-lg overflow-hidden">
