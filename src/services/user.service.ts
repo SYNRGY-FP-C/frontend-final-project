d5a0f993fb3e15c3cda78aa991c8d4bf41f69aa4
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2023-01-20 00:30:54 +0800

    update integration

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,23 +1,23 @@
 import { backendJava } from "../utils/axios";
 
 const registerPencari = async (data) => {
-  return await backendJava.post("/v1/auth/register/pencari", data);
+  return await backendJava.post("/v1/auth/pencari/register", data);
 };
 
 const registerPenyedia = async (data) => {
-  return await backendJava.post("/v1/auth/register/penyedia", data);
+  return await backendJava.post("/v1/auth/pemilik/register", data);
 };
 
 const loginPencari = async (data) => {
-  return await backendJava.post("/v1/auth/login/pencari", data);
+  return await backendJava.post("/v1/auth/login", data);
 };
 
 const loginPenyedia = async (data) => {
-  return await backendJava.post("/v1/auth/login/penyedia", data);
+  return await backendJava.post("/v1/auth/login", data);
 };
 
 const me = async () => {
-  return await backendJava.get("/v1/auth/currentuser");
+  return await backendJava.get("/v1/account/profile");
 };
 
 const userService = {
