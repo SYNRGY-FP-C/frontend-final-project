5b9ed524e1e2a664019e11a9be37c747a6e1f478
Abdul Jalil Nawawi <ajnawawi@gmail.com>
2023-01-20 21:45:33 +0700

    coba fetch sama loop

--- src/pages/my/history.jsx
+++ src/pages/my/history.jsx
@@ -3,7 +3,7 @@ import Button from "@/components/buttons/Button";
 import Defaultlayout from "@/layouts/DefaultLayout";
 import Section from "@/layouts/Section";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function History() {
   // gambaran buat response handle
@@ -14,17 +14,28 @@ export default function History() {
     data: [],
   });
 
-  //  useEffect(()=>{
+  useEffect(() => {
+    const fetchData = async () => {
+      setReponse({
+        isLoading: true,
+        isError: false,
+        data: [],
+      });
+      try {
+        const data = await historyService.getAll();
+        setReponse({
+          isLoading: false,
+          isError: false,
+          data: data,
+        });
+      } catch (error) {
+        setReponse({ isLoading: false, isError: error, data: [] });
+      }
+    };
 
-  //   setReponse({isLoading:true, isError:false, data :[]})
-  //   try {
-  //   const data =  historyService.getAll()
-  //   setReponse({isLoading:false, isError:false, data:data})
-  //   } catch (error) {
+    fetchData();
+  }, []);
 
-  //   setReponse({isLoading:false, isError:error, data:[]})
-  //   }
-  // },[])
   return (
     <Defaultlayout title="Riwayat">
       <Section>
@@ -58,115 +69,111 @@ export default function History() {
                   </Button>
                 </div>
               </div>
-              {/* 
-            // contoh looping
-            {
-              response.data.map((transaksi)=>{
-                return (<div className="grid col-span-4">
-                <div className="flex justify-center object-cover w-full h-64 overflow-hidden">
-                  <img
-                    className="object-cover w-full rounded-t-xl lg:rounded-l-xl lg:rounded-r-none"
-                    src={transaksi.image_url}
-                    alt="Test"
-                  />
-                </div>
-              </div>)
-              })
-            } */}
-              <div className="flex flex-col w-full gap-y-3">
-                <div className="grid grid-cols-1 shadow lg:grid-cols-12 rounded-xl">
-                  <div className="grid col-span-4">
-                    <div className="flex justify-center object-cover w-full h-64 overflow-hidden">
-                      <img
-                        className="object-cover w-full rounded-t-xl lg:rounded-l-xl lg:rounded-r-none"
-                        src="/images/hero-image.jpg"
-                        alt="Test"
-                      />
-                    </div>
-                  </div>
-                  <div className="grid col-span-8 p-5">
-                    <div className="relative flex flex-col h-full gap-y-3">
-                      <div className="flex flex-row justify-between">
-                        <h5 className="text-2xl font-bold text-primary-1">
-                          Kost Lorem
-                        </h5>
-                        <div className="flex flex-row gap-x-3">
-                          <span className="inline-flex items-center px-4 text-xs text-center bg-gray-200 rounded-lg">
-                            Superkost
-                          </span>
-                          <div className="w-8 h-8 rounded-lg bg-primary-1"></div>
+              <div>
+                {response.data.map((transaksi) => {
+                  return (
+                    <div className="grid grid-cols-1 shadow lg:grid-cols-12 rounded-xl">
+                      <div className="grid col-span-4">
+                        <div className="flex justify-center object-cover w-full h-64 overflow-hidden">
+                          <img
+                            className="object-cover w-full rounded-t-xl lg:rounded-l-xl lg:rounded-r-none"
+                            src={transaksi.image_url}
+                            alt="Test"
+                          />
                         </div>
                       </div>
-                      <div className="flex flex-row items-center gap-x-3">
-                        <div className="w-5 h-5 rounded-lg bg-primary-1"></div>
-                        <p>Kecamatan Lorem, Bandung</p>
-                      </div>
-                      <div className="flex flex-row items-center gap-x-3">
-                        <span className="w-24 text-center py-0.5 border border-gray-300 text-primary-1 rounded-lg">
-                          Campur
-                        </span>
-                        <div className="inline-flex items-center gap-x-1">
-                          <div className="w-5 h-5 rounded-lg bg-primary-1"></div>{" "}
-                          5
+                      <div className="grid col-span-8 p-5">
+                        <div className="relative flex flex-col h-full gap-y-3">
+                          <div className="flex flex-row justify-between">
+                            <h5 className="text-2xl font-bold text-primary-1">
+                              {transaksi.data_nama}
+                            </h5>
+                            <div className="flex flex-row gap-x-3">
+                              <span className="inline-flex items-center px-4 text-xs text-center bg-gray-200 rounded-lg">
+                                Superkost
+                              </span>
+                              <div className="w-8 h-8 rounded-lg bg-primary-1"></div>
+                            </div>
+                          </div>
+                          <div className="flex flex-row items-center gap-x-3">
+                            <div className="w-5 h-5 rounded-lg bg-primary-1"></div>
+                            <p>{transaksi.data_alamat}</p>
+                          </div>
+                          <div className="flex flex-row items-center gap-x-3">
+                            <span className="w-24 text-center py-0.5 border border-gray-300 text-primary-1 rounded-lg">
+                              {transaksi.data_tipe}
+                            </span>
+                            <div className="inline-flex items-center gap-x-1">
+                              <div className="w-5 h-5 rounded-lg bg-primary-1"></div>{" "}
+                              5
+                            </div>
+                          </div>
+                          <div className="flex items-stretch justify-end h-full">
+                            <p className="self-end text-xl font-bold text-primary-1">
+                              {transaksi.data_harga}
+                            </p>
+                          </div>
                         </div>
                       </div>
-                      <div className="flex items-stretch justify-end h-full">
-                        <p className="self-end text-xl font-bold text-primary-1">
-                          Lorem ipsum dolor / bulan
-                        </p>
-                      </div>
                     </div>
-                  </div>
-                </div>
+                  );
+                })}
+              </div>
 
-                <div className="grid grid-cols-1 shadow lg:grid-cols-12 rounded-xl">
-                  <div className="grid col-span-4">
-                    <div className="flex justify-center object-cover w-full h-64 overflow-hidden">
-                      <img
-                        className="object-cover w-full rounded-t-xl lg:rounded-l-xl lg:rounded-r-none"
-                        src="/images/hero-image.jpg"
-                        alt="Test"
-                      />
-                    </div>
-                  </div>
-                  <div className="grid col-span-8 p-5">
-                    <div className="relative flex flex-col h-full gap-y-3">
-                      <div className="flex flex-row justify-between">
-                        <h5 className="text-2xl font-bold text-primary-1">
-                          Kost Lorem
-                        </h5>
-                        <div className="flex flex-row gap-x-3">
-                          <span className="inline-flex items-center px-4 text-xs text-center bg-gray-200 rounded-lg">
-                            Superkost
-                          </span>
-                          <div className="w-8 h-8 rounded-lg bg-primary-1"></div>
+              <div className="flex flex-col w-full gap-y-3">
+                <div>
+                  {response.data.map((transaksi) => {
+                    return (
+                      <div className="grid grid-cols-1 shadow lg:grid-cols-12 rounded-xl">
+                        <div className="grid col-span-4">
+                          <div className="flex justify-center object-cover w-full h-64 overflow-hidden">
+                            <img
+                              className="object-cover w-full rounded-t-xl lg:rounded-l-xl lg:rounded-r-none"
+                              src={transaksi.image_url}
+                              alt="Test"
+                            />
+                          </div>
+                        </div>
+                        <div className="grid col-span-8 p-5">
+                          <div className="relative flex flex-col h-full gap-y-3">
+                            <div className="flex flex-row justify-between">
+                              <h5 className="text-2xl font-bold text-primary-1">
+                                {transaksi.data_nama}
+                              </h5>
+                              <div className="flex flex-row gap-x-3">
+                                <span className="inline-flex items-center px-4 text-xs text-center bg-gray-200 rounded-lg">
+                                  Superkost
+                                </span>
+                                <div className="w-8 h-8 rounded-lg bg-primary-1"></div>
+                              </div>
+                            </div>
+                            <div className="flex flex-row items-center gap-x-3">
+                              <div className="w-5 h-5 rounded-lg bg-primary-1"></div>
+                              <p>{transaksi.data_alamat}</p>
+                            </div>
+                            <div className="flex flex-row items-center gap-x-3">
+                              <span className="w-24 text-center py-0.5 border border-gray-300 text-primary-1 rounded-lg">
+                                {transaksi.data_tipe}
+                              </span>
+                              <div className="inline-flex items-center gap-x-1">
+                                <div className="w-5 h-5 rounded-lg bg-primary-1"></div>{" "}
+                                5
+                              </div>
+                            </div>
+                            <div className="flex items-stretch justify-end h-full">
+                              <p className="self-end text-xl font-bold text-primary-1">
+                                {transaksi.data_harga}
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                      <div className="flex flex-row items-center gap-x-3">
-                        <div className="w-5 h-5 rounded-lg bg-primary-1"></div>
-                        <p>Kecamatan Lorem, Bandung</p>
-                      </div>
-                      <div className="flex flex-row items-center gap-x-3">
-                        <span className="w-24 text-center py-0.5 border border-gray-300 text-primary-1 rounded-lg">
-                          Campur
-                        </span>
-                        <div className="inline-flex items-center gap-x-1">
-                          <div className="w-5 h-5 rounded-lg bg-primary-1"></div>{" "}
-                          5
-                        </div>
-                      </div>
-                      <div className="flex items-stretch justify-end h-full">
-                        <p className="self-end text-xl font-bold text-primary-1">
-                          Lorem ipsum dolor / bulan
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </div>
       </Section>
     </Defaultlayout>
