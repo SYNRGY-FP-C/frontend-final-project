ce9ac97178fd3af2d1e6f02bb13a2523c35b24aa
Khoironi Kurnia Syah <55708473+zekhoi@users.noreply.github.com>
2023-02-07 22:56:20 +0800

    Feat/dashboardpm (#35)
    * finishing for showcase
    
    * fix error
    
    * update button
    
    * sementara
    
    * test
    
    * test
    
    * sementara
    
    * gitulah

--- src/layouts/SidebarDashboard.jsx
+++ src/layouts/SidebarDashboard.jsx
@@ -1,55 +1,37 @@
+import SidebarItem from "@/components/links/SidebarItem";
+import { dashboardMenu } from "@/constants/menu";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import Navbar from "@/layouts/Navbar";
+import { v4 as uuid } from "uuid";
 
-export default function Sidebar({ children }) {
-  const router = useRouter();
-
-  const menuItems = [
-    {
-      href: "/dashboard",
-      title: "Home",
-    },
-    {
-      href: "/dashboard/transaksi",
-      title: "Transaksi",
-    },
-    {
-      href: "/dashboard/history",
-      title: "Riwayat",
-    },
-  ];
-
+export default function Sidebar() {
   return (
-    <div className="min-h-screen flex flex-col fixed">
-      <div className="flex flex-col md:flex-row flex-1">
-        <aside className="bg-primary-1 w-80">
+    <div className="flex-col hidden min-h-screen lg:flex">
+      <div className="flex flex-col flex-1 md:flex-row">
+        <aside className="py-6 bg-primary-1 w-80 gap-y-6">
           <Image
             src="/images/Brand.png"
             alt="logo"
-            className="mx-auto mt-20 mb-12"
+            className="mx-auto"
             width={190}
             height={190}
           />
-          <nav className="ml-9">
-            <div className="mr-9 mb-9 px-9 py-6 bg-white text-center rounded-lg hover:bg-slate-300">
-              <Link href="/dashboard/kost" className="flex items-center gap-1">
-                <Image src="/images/add_box.svg" width={24} height={24}></Image>
+          <nav className="my-6 ml-9 gap-y-3">
+            <div className="py-6 text-center bg-white rounded-lg mr-9 mb-9 px-9 hover:bg-slate-300">
+              <Link href="/dashboard/kost/add" className="flex items-center">
+                <Image
+                  src="/images/add_box.svg"
+                  alt="KostHub"
+                  width={24}
+                  height={24}
+                />
                 <h1>Tambah Kos Baru</h1>
               </Link>
             </div>
-            <ul>
-              {menuItems.map(({ href, title }) => (
-                <li className="" key={title}>
-                  <Link
-                    href={href}
-                    className={`flex py-6 pl-9 bg-primary-1 text-white rounded-l-lg hover:bg-white cursor-pointer hover:text-primary-1 ${
-                      router.asPath === href && " text-primary-1 bg-white"
-                    }`}
-                  >
-                    {title}
-                  </Link>
+            <ul className="flex flex-col gap-y-3">
+              {dashboardMenu.map((menu) => (
+                <li className="flex w-full" key={uuid()}>
+                  <SidebarItem href={menu.href} label={menu.title} />
                 </li>
               ))}
             </ul>
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,98 +1,58 @@
+/* eslint-disable @next/next/no-img-element */
+import KostCard from "@/components/cards/KostCard";
 import Carrousel from "@/components/Carrousel";
-import Location from "@/components/icons/Location";
-import Star from "@/components/icons/Star";
+import { ROLE_ADMIN } from "@/constants/roles";
+import DashboardLayout from "@/layouts/DashboardLayout";
 import ProtectedPage from "@/layouts/ProtectedPage";
-import Sidebar from "@/layouts/SidebarDashboard";
-import Link from "next/link";
+import { useState } from "react";
 
-export default function index() {
+export default function Dashboard() {
+  const [statistic, setStatistic] = useState({
+    bookers: 0,
+    occupants: 0,
+    empty_rooms: 0,
+  });
+
   return (
-    <ProtectedPage allowed={["ROLE_USER_PEMILIK"]} redirect="/401">
-      <div className="flex flex-col flex-wrap ">
-        <div class="flex">
-          <Sidebar />
-          <div className="w-full max-w-screen-xl h-full ml-80">
-            <div className="w-4/5 mx-auto my-10">
-              <Carrousel />
-              <div className="my-12">
-                <h1 className="text-2xl font-bold">Statistik</h1>
-                <div className="w-full mt-3 flex ">
-                  <div className="w-1/4 mr-4 border rounded-md shadow-lg h-32">
-                    <div className="my-7 ml-7">
-                      <h1 className="text-3xl text-primary-2 font-bold">5</h1>
-                      <h2 className="text-lg">Janji Temu</h2>
-                    </div>
-                  </div>
-                  <div className="w-1/4 mr-4 border rounded-md shadow-lg h-32">
-                    <div className="my-7 ml-7">
-                      <h1 className="text-3xl text-primary-2 font-bold">3</h1>
-                      <h2 className="text-lg">Pemesan</h2>
-                    </div>
-                  </div>
-                  <div className="w-1/4 mr-4 border rounded-md shadow-lg h-32">
-                    <div className="my-7 ml-7">
-                      <h1 className="text-3xl text-primary-2 font-bold">5</h1>
-                      <h2 className="text-lg">Penghuni</h2>
-                    </div>
-                  </div>
-                  <div className="w-1/4 mr-4 border rounded-md shadow-lg h-32">
-                    <div className="my-7 ml-7">
-                      <h1 className="text-3xl text-primary-2 font-bold">5</h1>
-                      <h2 className="text-lg">Kamar Kosong</h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="pb-10">
-                <h1 className="text-2xl font-bold">Kos Anda</h1>
-                <div className="flex ">
-                  <Link
-                    href="/dashboard"
-                    className="w-1/2 border rounded-lg hover:shadow-xl shadow-primary-2"
-                  >
-                    <img
-                      src="/images/hero.png"
-                      alt=""
-                      className="rounded-t-lg w-full h-32"
-                    />
-                    <div className="m-4">
-                      <div className="flex place-content-between items-center">
-                        <h1 className="font-bold">Kos Binar</h1>
-                        <h1 className="text-xs border border-black rounded-full px-6 py-2">
-                          Putra
-                        </h1>
-                      </div>
-                      <div className="flex items-center">
-                        <Location className="w-4" />
-                        <h1 className="ml-3">Kecamatan Lorem, Bandung</h1>
-                      </div>
-                      <div className="flex items-center place-content-between my-5">
-                        <div className=" bg-secondary-1 rounded-full">
-                          <h1 className="py-1 px-3 text-white text-xs">
-                            Superkost
-                          </h1>
-                        </div>
-                        <div>
-                          <div className="flex items-center">
-                            <Star className="w-6" />
-                            <h1 className="">5</h1>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
-                  <div className="w-1/2 h-72 border border-black rounded-lg ml-5 items-center">
-                    <h1 className="text-center mt-36">
-                      Anda belum memiliki kost lain
-                    </h1>
-                  </div>
-                </div>
-              </div>
+    <ProtectedPage allowed={[ROLE_ADMIN]} redirect="/403">
+      <DashboardLayout title="Dashboard - Pemilik">
+        <div className="flex flex-col gap-y-2">
+          <div className="flex items-center justify-center w-full max-w-4xl mx-auto lg:p-6 2xl:max-w-6xl">
+            <Carrousel />
+          </div>
+          <h1 className="text-2xl font-bold">Statistik</h1>
+          <div className="grid grid-cols-1 gap-3 lg:grid-cols-3">
+            <div className="h-32 p-6 border rounded-lg border-base-5">
+              <h5 className="text-3xl font-bold text-primary-2">
+                {statistic.bookers}
+              </h5>
+              <p className="text-lg">Pemesan</p>
+            </div>
+            <div className="h-32 p-6 border rounded-lg border-base-5">
+              <h5 className="text-3xl font-bold text-primary-2">
+                {statistic.occupants}
+              </h5>
+              <p className="text-lg">Penghuni</p>
+            </div>
+            <div className="h-32 p-6 border rounded-lg border-base-5">
+              <h5 className="text-3xl font-bold text-primary-2">
+                {statistic.empty_rooms}
+              </h5>
+              <p className="text-lg">Kamar Kosong</p>
             </div>
           </div>
         </div>
-      </div>
+
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-bold">Kos Anda</h1>
+          <div className="grid grid-cols-1 gap-3 lg:grid-cols-2">
+            <KostCard />
+            <div className="flex items-center justify-center border rounded-lg border-base-2 h-72">
+              <h1 className="text-center">Anda belum memiliki kost lain</h1>
+            </div>
+          </div>
+        </div>
+      </DashboardLayout>
     </ProtectedPage>
   );
 }
