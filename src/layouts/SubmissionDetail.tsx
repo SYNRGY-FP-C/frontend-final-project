290a649a2526f057f2526e2b71beeded6bf86547
Andri Wahyudi <95180213+andriwah@users.noreply.github.com>
2023-02-09 18:09:48 +0700

    Feature/detail room (#42)
    * add response, fetcdata
    
    * change import service
    
    * change import service
    
    * change err
    
    * add image tab
    
    * revisi halaman details room, dan pengajuan sewa
    
    * add alert response
    
    * detail room and submission page
    
    * add margin-top breadcump submission
    
    * .
    
    * room id router
    
    * delete img ktp
    
    * login
    
    * .
    
    * .
    
    * add preview image
    
    * rounded image card
    
    * revisi typo
    
    * integrasi API get by ID kamar
    
    * add getserver static prop detailroom & submission
    
    * add button simpan favorite, transaction service
    
    * transaction integrasi
    
    * add isLoading

--- src/layouts/SubmissionDetail.tsx
+++ src/layouts/SubmissionDetail.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 export default function SubmissionDetail({ title = "Title", children }) {
   return (
     <div className="grid grid-cols-1 gap-4 my-4 lg:grid-cols-3">
       <div className="col-span-1">
-        <h5 className="text-2xl font-semibold text-primary-1">{title}</h5>
+        <div className="inline-flex items-center gap-x-2">
+          <h5 className="text-2xl font-semibold text-primary-1">{title}</h5>
+          {title === "Profil Pencari" ? (
+            <Link href="/my" className="mt-2 mr-4 underline">
+              Ubah
+            </Link>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
       <div className="col-span-2">{children}</div>
     </div>
